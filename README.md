4a800eb6aeadbb305e29863eeb3975172c386455
Tony Pai <tony77794@gmail.com>
2019-09-06 02:01:32 +0800

    Init commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Oriente UI
+
+> A bunch of components for supporting Oriente front-end team.
+
+[DEMO](https://oriente-ui.github.io/)
+
+## Setup
+
+```bash
+$ npm install vcolorpicker -S
+```
+## Usage
+
+```js
+import orienteUI from 'oriente-ui'
+Vue.use(orienteUI)
+```
+
+## Build
+
+Publish npm package
+
+```bash
+$ yarn lib
+```
+
+## Development
+
+Start styleguidist
+
+```bash
+$ yarn styleguide
+```
+
+Start demo site
+
+```bash
+$ yarn serve
+```
---
+++ vue.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+
+module.exports = {
+  pages: {
+    index: {
+      entry: 'examples/main.js',
+      template: 'public/index.html',
+      filename: 'index.html'
+    }
+  },
+  css: { extract: false },
+  configureWebpack: {
+    resolve: {
+      extensions: ['.js', '.json', '.vue'],
+      alias: {
+        '~': path.resolve(__dirname),
+        '@': path.resolve(__dirname, 'examples')
+      }
+    }
+  },
+  chainWebpack: config => {
+    config.module
+      .rule('js')
+      .include
+        .add(path.resolve(__dirname, 'packages'))
+        .end()
+      .use('babel')
+        .loader('babel-loader')
+        .tap(options => {
+          return options
+        })
+  }
+}
