20208dc79a6050e50b94580efd7873661fb956cc
Tony Pai <tony77794@gmail.com>
2019-09-09 14:29:03 +0800

    Enable css extraction for SSR usage

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
       filename: 'index.html'
     }
   },
-  css: { extract: false },
+  css: { extract: true },
   configureWebpack: {
     resolve: {
       extensions: ['.js', '.json', '.vue'],
