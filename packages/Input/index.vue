5b8dfe3995ae5f288194e30c85f36b5c24b38323
Tony Pai <tony77794@gmail.com>
2020-01-09 18:45:57 +0800

    Update Input
    - Add new prop styleType
    - Fix prop value for two-way binding
    - Ignore invalid-end-tag eslint rule

--- packages/Input/index.vue
+++ packages/Input/index.vue
@@ -2,14 +2,31 @@
   <div :class="$style['container']">
     <label :class="$style['label']">
       {{ label }}
-      <input
-        :type="type"
-        :class="[$style['input'], { [$style['error']]: error }]"
-        :placeholder="placeholder"
-        @change="handleChange"
-        @keyup="handleKeyup"
-        v-model="value"
-      />
+      <div :class="[$style['input'], $style[`${styleType}-bg`]]">
+        <div
+          v-if="$slots.prependIcon"
+          :class="[$style['icon'], $style['prepend']]"
+        >
+          <!-- @slot prepend icon -->
+          <slot name="prependIcon" />
+        </div>
+        <input
+          :type="type"
+          :class="[
+            $style['input-self'],
+            $style[styleType],
+            { [$style['error']]: error }
+          ]"
+          :placeholder="placeholder"
+          @change="handleChange"
+          @keyup="handleKeyup"
+          v-model="internalValue"
+        />
+        <div v-if="$slots.appendIcon" :class="$style['icon']">
+          <!-- @slot append icon -->
+          <slot name="appendIcon" />
+        </div>
+      </div>
     </label>
     <div v-if="error" :class="$style['error-text']">{{ error }}</div>
   </div>
@@ -27,6 +44,13 @@ export default {
       default: 'text'
     },
     /**
+     * style type (e.g. regular, box)
+     */
+    styleType: {
+      type: String,
+      default: 'regular'
+    },
+    /**
      * label text
      */
     label: {
@@ -41,9 +65,9 @@ export default {
       default: ''
     },
     /**
-     * default value
+     * two way value
      */
-    defaultValue: {
+    value: {
       type: String,
       default: ''
     },
@@ -57,8 +81,23 @@ export default {
   },
   data() {
     return {
-      value: this.defaultValue,
+      lazyValue: this.value,
       error: ''
+    }
+  },
+  computed: {
+    internalValue: {
+      get() {
+        return this.lazyValue
+      },
+      set(val) {
+        this.lazyValue = val
+      }
+    }
+  },
+  watch: {
+    value(val) {
+      this.lazyValue = val
     }
   },
   methods: {
@@ -104,6 +143,8 @@ export default {
 .container {
   @extend %flex-default;
   flex-flow: column nowrap;
+
+  width: 100%;
 }
 
 .label {
@@ -113,21 +154,60 @@ export default {
 }
 
 .input {
+  @extend %flex-default;
+  align-items: center;
+
   width: 100%;
+}
+
+.input-self {
+  width: 100%;
+  border: 0;
+  background: transparent;
+  outline: none;
+  box-shadow: none;
+}
+
+.regular {
   color: $font-color;
   font-size: $fs-16;
+
+  &::placeholder {
+    color: $black-30;
+  }
+}
+
+.regular-bg {
+  width: 100%;
+  background: transparent;
   padding: $spacing-2 0;
   border: 0;
   border-radius: 0;
   border-bottom: 1px solid $gray;
-  outline: none;
 
-  &:focus {
+  &:focus-within {
     border-color: $black-09;
   }
+}
+
+.box {
+  color: $font-color;
+  font-size: $fs-16;
 
   &::placeholder {
     color: $black-30;
+  }
+}
+
+.box-bg {
+  width: calc(100% - #{2 * $spacing-2});
+  background: $black-09;
+  padding: $spacing-1;
+  border: 0;
+  border-radius: 5px;
+
+  &:focus-within {
+    background: #eee;
   }
 }
 
@@ -139,6 +219,18 @@ export default {
   color: $danger;
   font-size: $fs-12;
   margin: 4px 0 0;
+}
+
+.icon {
+  @extend %flex-center;
+
+  width: 25px;
+  height: 25px;
+  user-select: none;
+}
+
+.prepend {
+  margin: 0 5px 0 0;
 }
 </style>
 
@@ -164,6 +256,19 @@ export default {
   }
 }
 </script>
+```
+
+Box
+
+```jsx
+<Input styleType="box" placeholder="Search something...">
+  <div slot="prependIcon">
+    <i class="fas fa-search"></i>
+  </div>
+  <div slot="appendIcon">
+    <i class="fas fa-times-circle"></i>
+  </div>
+</Input>
 ```
 
 With validation
